b915718e052b4b3d7936b593fbe35b3027796c55
oyrad <dario.susanj2@gmail.com>
2024-05-23 00:01:52 +0200

    add video

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
 export default function Home() {
-  return <div id="background-image" />;
+  return (
+    <section>
+      <video muted loop autoPlay id="cover-video" className="md:hidden">
+        <source src="/videos/cover-mobile.mp4" type="video/mp4" />
+      </video>
+      <video muted loop autoPlay id="cover-video" className="hidden md:block">
+        <source src="/videos/cover-desktop.mp4" type="video/mp4" />
+      </video>
+    </section>
+  );
 }
