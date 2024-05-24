663f4486783c8d0ab28bef0e5de9fd5c9c1ad6a1
oyrad <dario.susanj2@gmail.com>
2024-05-24 21:22:01 +0200

    add playsInLine to video

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,24 @@
 export default function Home() {
   return (
     <section>
-      <video muted loop autoPlay id="cover-video" className="md:hidden">
+      <video
+        muted
+        loop
+        autoPlay
+        playsInline
+        id="cover-video"
+        className="md:hidden"
+      >
         <source src="/videos/cover-mobile.mp4" type="video/mp4" />
       </video>
-      <video muted loop autoPlay id="cover-video" className="hidden md:block">
+      <video
+        muted
+        loop
+        autoPlay
+        playsInline
+        id="cover-video"
+        className="hidden md:block"
+      >
         <source src="/videos/cover-desktop.mp4" type="video/mp4" />
       </video>
     </section>
