28d373d404905ac361491d9644bf3b430ae4c53f
oyrad <dario.susanj2@gmail.com>
2024-06-04 00:14:38 +0200

    make video autoplay on ios

--- app/page.tsx
+++ app/page.tsx
@@ -22,10 +22,24 @@ export default function Home() {
     <>
       {!isTimeoutDone && <SplashScreen fadeOut={fadeOut} />}
       <section className="-mt-16">
-        <video muted loop autoPlay id="cover-video" className="md:hidden">
+        <video
+          muted
+          loop
+          autoPlay
+          playsInline
+          id="cover-video"
+          className="md:hidden"
+        >
           <source src="/videos/cover-mobile.mp4" type="video/mp4" />
         </video>
-        <video muted loop autoPlay id="cover-video" className="hidden md:block">
+        <video
+          muted
+          loop
+          autoPlay
+          playsInline
+          id="cover-video"
+          className="hidden md:block"
+        >
           <source src="/videos/cover-desktop.mp4" type="video/mp4" />
         </video>
       </section>
